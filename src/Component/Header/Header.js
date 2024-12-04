18ae7e65d6ff7c702f95567892f895428357c1f8
Aryansahu <aryansahuosvn2006@gmail.com>
2024-12-04 19:05:29 +0530

    starting

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Logo from '../../Image/logo.png'
+import './Header.css'
+const Header = () => {
+    const Navigate = useNavigate();
+    const About = (item) => {
+      Navigate(item);
+    };
+  return (
+    <>
+        
+    <div className=" navrelative">
+      <Navbar expand="lg navbarcolor">
+        <Container>
+          <Navbar.Brand href="./" >
+            <img src={Logo} className='logo' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link to="#" onClick={() => About("./")} className="navhover">
+                Home
+              </Nav.Link>
+              <Nav.Link
+                to="#"
+                onClick={() => About("./About")}
+                className="navhover"
+              >
+                About
+              </Nav.Link>
+              <Nav.Link
+                to="#"
+                onClick={() => About("./Services")}
+                className="navhover"
+              >
+                Services
+              </Nav.Link>
+              <Nav.Link
+                to="#"
+                onClick={() => About("./Contact")}
+                className="navhover"
+              >
+                Contact
+              </Nav.Link>
+              <div className="center">
+                <Button variant="primary btn-num">202-555-0188</Button>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+ 
+
+
+
+
+
+    </>
+  )
+}
+
+export default Header
---
+++ src/Component/Heading/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Heading.css'
+const Heading = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Heading
